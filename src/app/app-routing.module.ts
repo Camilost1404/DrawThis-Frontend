501d9abbd79ae55cb27fc0a451c86ca941ba58af
Camilost1404 <camitorres1404@gmail.com>
2021-05-13 16:53:31 -0500

    Rutas y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RoomComponent } from './room/room.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Declaramos rutas de la Pagina
+  {
+    // Ruta que referecia los componentes
+    path: '', // Ruta raíz ( https://localhost:4200/)
+
+    // Pasandole el componente que va a usarse en la ruta
+    component: HomeComponent,
+  },
+  {
+    path: ':room', // Ruta dinamica, es decir con una variable para hacer referencia a las salas
+    component: RoomComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/room/room.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-room',
+  templateUrl: './room.component.html',
+  styleUrls: ['./room.component.css']
+})
+export class RoomComponent implements OnInit {
+
+  room: any;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.room = this.route.snapshot.paramMap.get("room");
+    console.log(this.room);
+  }
+
+}
